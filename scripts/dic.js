c7845d6421a9f5541b667cafc45fe21cc5a1fbfa
Ryan Beckett <beckett.ryan@gmail.com>
2012-11-08 06:02:16 -0500

    1. Implemented login function via a POST ajax call. Any errors will be logged.

--- scripts/dic.js
+++ scripts/dic.js
@@ -1,21 +1,44 @@
 var DIC = {
-    GetUnreadSubscriptions: function(callback){
-        $.ajax({
-            url: Config.Settings.subsUrl,
-            dataType: 'html',
-            cache: false
-        }).done(function (res) {
-            var $res = $(res);
-            if (($res).find('input#username').length > 0 && callback) {
-                callback(null);
-                return;
-            }
-            var unread = $res.find(Config.Settings.unreadSelector);
-            var rows = unread.parent().parent();
-            if (callback) callback(rows);
-        }).fail(function (xhr, status){
-		    console.log(status);
-		    Tools.setErrorBadge();
-        });
-    }
+	GetUnreadSubscriptions : function(callback) {
+		$.ajax({
+			url : Config.Settings.subsUrl,
+			dataType : 'html',
+			cache : false
+		}).done(function(res) {
+			var $res = $(res);
+			if (($res).find('input#username').length > 0 && callback) {
+				callback(null);
+				return;
+			}
+			var unread = $res.find(Config.Settings.unreadSelector);
+			var rows = unread.parent().parent();
+			if (callback)
+				callback(rows);
+		}).fail(function(xhr, status) {
+			console.log(status);
+			Tools.setErrorBadge();
+		});
+	},
+
+	LogIn : function(user, pass, remember) {
+		$('#alert-error').hide();
+		$.ajax({
+			type : 'POST',
+			url : Config.Settings.loginUrl,
+			data : {
+				'referer' : Config.Settings.referer,
+				'username' : user,
+				'password' : pass,
+				'rememberMe' : remember,
+			},
+			dataType : 'html',
+			error : function(jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR.statusText);
+				Tools.setErrorBadge();
+			},
+			complete : function(jqXHR, textStatus) {
+				DIC.GetUnreadSubscriptions(subscriptionHandler);
+			}
+		});
+	}
 };
