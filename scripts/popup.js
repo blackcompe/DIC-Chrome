de0241e5b832f975a4bf395dae4478a5a0d35d97
Ryan Beckett <beckett.ryan@gmail.com>
2012-11-08 05:59:50 -0500

    1. Refactored anonymous document.onReady callback to its function for reuse. It's named subscriptionHandler().
    2. Created a login handler for the new login form.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,46 +1,64 @@
 function makeLinksOpenInTabs() {
-    $('a').on('click', function () {
-        if ($(this).attr('href') == '#') return;
-        Tools.clearBadge();
-        Tools.createTab($(this).attr('href'));
-    });
+	$('a').on('click', function() {
+		if ($(this).attr('href') == '#')
+			return;
+		Tools.clearBadge();
+		Tools.createTab($(this).attr('href'));
+	});
 }
 
 function setOpenAllLink(links) {
-    $('#open-all').on('click', function () {
-        Tools.clearBadge();
-        for (var i = 0; i < links.length; i++) {
-            Tools.createTab(links[i].attr('href'));
-        }
-    });
+	$('#open-all').on('click', function() {
+		Tools.clearBadge();
+		for ( var i = 0; i < links.length; i++) {
+			Tools.createTab(links[i].attr('href'));
+		}
+	});
 }
 
-$(document).on('ready', function () {
-    $('#loader').show();
-    Tools.setLoadingBadge();
-    DIC.GetUnreadSubscriptions(function (unread) {
-        $('#loader').hide();
-        Tools.clearBadge();
-        if (!unread) {
-            $('#alert-error').show();
-            Tools.setErrorBadge();
-        }
-        else if (unread.length < 1) {
-            $('#alert-info').show();
-        }
-        else {
-            $('#entries').show();
-            var list = $('#entries').find('ul');
-            var links = new Array();
-            unread.find(Config.Settings.linkSelector).each(function () {
-                list.append($('<li></li>').append($(this)));
-                links.push($(this));
-            });
-            list.append($('<li class="divider"></li>'));
-            list.append($('<li><a href="#" id="open-all">Open All</a></li>'));
-            setOpenAllLink(links);
-        }
+function setLoginHandler() {
+	$('#login-btn').click(function() {
+		$('#login-form').hide();
+		Tools.setLoadingBadge();
+		$('#login-loader').show();
+		var remember;
+		if ($('#remember').is(':checked'))
+			remember = 1;
+		else
+			remember = 0;
+		DIC.LogIn($('#username').val(), $('#password').val(), remember);
+		return false;
+	});
+}
 
-        makeLinksOpenInTabs();
-    });
+function subscriptionHandler(unread) {
+	$('#login-loader').hide();
+	$('#loader').hide();
+	Tools.clearBadge();
+	if (!unread) {
+		$('#alert-error').show();
+		Tools.setErrorBadge();
+		$('#login-form').show();
+		setLoginHandler();
+	} else if (unread.length < 1) {
+		$('#alert-info').show();
+	} else {
+		$('#entries').show();
+		var list = $('#entries').find('ul');
+		var links = new Array();
+		unread.find(Config.Settings.linkSelector).each(function() {
+			list.append($('<li></li>').append($(this)));
+			links.push($(this));
+		});
+		list.append($('<li class="divider"></li>'));
+		list.append($('<li><a href="#" id="open-all">Open All</a></li>'));
+		setOpenAllLink(links);
+	}
+	makeLinksOpenInTabs();
+}
+
+$(document).on('ready', function() {
+	$('#loader').show();
+	Tools.setLoadingBadge();
+	DIC.GetUnreadSubscriptions(subscriptionHandler);
 });
